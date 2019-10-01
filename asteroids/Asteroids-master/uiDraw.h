d956df028bc444c58bd03fa80f60182d61c699fe
wheelerkeith <nemimimonkey@gmail.com>
2019-10-01 11:45:58 -0500

    changes

---
+++ asteroids/Asteroids-master/uiDraw.h
@@ -0,0 +1,92 @@
+/***********************************************************************
+ * Header File:
+ *    User Interface Draw : put pixels on the screen
+ * Author:
+ *    Br. Helfrich
+ * Summary:
+ *    This is the code necessary to draw on the screen. We have a collection
+ *    of procedural functions here because each draw function does not
+ *    retain state. In other words, they are verbs (functions), not nouns
+ *    (variables) or a mixture (objects)
+ ************************************************************************/
+
+#ifndef UI_DRAW_H
+#define UI_DRAW_H
+
+#include <string>     // To display text on the screen
+#include <cmath>      // for M_PI, sin() and cos()
+#include "point.h"    // Where things are drawn
+using std::string;
+
+/************************************************************************
+ * DRAW DIGIT
+ * Draw a single digit in the old school line drawing style.  The
+ * size of the glyph is 8x11 or x+(0..7), y+(0..10)
+ *************************************************************************/
+void drawDigit(const Point & topLeft, char digit);
+
+/*************************************************************************
+ * DRAW NUMBER
+ * Display an positive integer on the screen using the 7-segment method
+ *************************************************************************/
+void drawNumber(const Point & topLeft, unsigned int number);
+
+/*************************************************************************
+ * DRAW TEXT
+ * Draw text using a simple bitmap font
+ ************************************************************************/
+void drawText(const Point & topLeft, const char * text);
+
+/************************************************************************
+ * DRAW RECTANGLE
+ * Draw a rectangle on the screen centered on a given point (center) of
+ * a given size (width, height), and at a given orientation (rotation)
+ * measured in degrees (0 - 360)
+ *************************************************************************/
+void drawRect(const Point & center, int width, int height, int rotation);
+
+/************************************************************************
+ * DRAW CIRCLE
+ * Draw a circle from a given location (center) of a given size (radius).
+ *************************************************************************/
+void drawCircle(const Point & center, int radius);
+void drawCircle(const Point & center, int radius, double r, double g, double b);
+
+/************************************************************************
+ * DRAW POLYGON
+ * Draw a polygon from a given location (center) of a given size (radius).
+ *************************************************************************/
+void drawPolygon(const Point & center, int radius, int points, int rotation);
+
+/************************************************************************
+ * DRAW LINE
+ * Draw a line on the screen from the beginning to the end.
+ *************************************************************************/
+void drawLine(const Point & begin, const Point & end);
+void drawLine(const Point & begin, const Point & end, double r, double g, double b);
+
+/************************************************************************
+ * DRAW DOT
+ * Draw a single point on the screen, 2 pixels by 2 pixels
+ *************************************************************************/
+void drawDot(const Point & point);
+void drawDot(const Point & point, double r, double g, double b);
+
+/************************************************************************
+ * DRAW Ship
+ * Draw the spaceship on the screen
+ *************************************************************************/
+void drawShip(const Point & point, int rotation);
+
+/******************************************************************
+ * RANDOM
+ * This function generates a random number.  The user specifies
+ * The parameters 
+ *    INPUT:   min, max : The number of values (min <= num <= max)
+ *    OUTPUT   <return> : Return the integer
+ ****************************************************************/
+int    random(int    min, int    max);
+double random(double min, double max);
+
+
+#endif // UI_DRAW_H
